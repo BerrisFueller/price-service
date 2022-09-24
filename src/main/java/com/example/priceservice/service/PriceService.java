551100a29d1e7c3cea1c7bd3c13d948d06ab3111
Melisa <melcafidan@gmail.com>
2022-09-24 19:57:52 +0200

    round prices to two decimals

--- src/main/java/com/example/priceservice/service/PriceService.java
+++ src/main/java/com/example/priceservice/service/PriceService.java
@@ -5,14 +5,17 @@ import com.example.priceservice.consumer.dto.PriceResponse;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Service
 public class PriceService {
 
 
-    public PriceResponse sumOfALlValues(PriceRequest priceRequest){
+    public PriceResponse sumOfALlValues(PriceRequest priceRequest) {
+        BigDecimal rawPrice = priceRequest.getPriceList().stream().reduce(BigDecimal.ZERO, BigDecimal::add);
+        BigDecimal roundedPrice = rawPrice.setScale(2, RoundingMode.CEILING);
 
-        return new PriceResponse().setPrice(priceRequest.getPriceList().stream().reduce(BigDecimal.ZERO, BigDecimal::add));
+        return new PriceResponse().setPrice(roundedPrice);
     }
 
 }
