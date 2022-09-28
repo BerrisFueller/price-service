bfd7313ed559cb24ff0da8659c70eced07702548
NasiemZ <nasiem2@hotmail.de>
2022-09-28 20:04:23 +0200

    Added some Junit Tests

--- src/test/java/com/example/priceservice/PriceServiceApplicationTests.java
+++ src/test/java/com/example/priceservice/PriceServiceApplicationTests.java
@@ -1,29 +1,82 @@
 package com.example.priceservice;
 
+import com.example.priceservice.consumer.dto.PriceRequest;
+import com.example.priceservice.consumer.dto.PriceResponse;
 import com.example.priceservice.service.PriceService;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class PriceServiceApplicationTests {
+            private final PriceService priceService = new PriceService();
 
-    private final PriceService priceService = new PriceService();
-    private final List<Integer> priceList = Arrays.asList(12,18,20,15,19,21);
+
+            @Test
+            public void CalculatePrice(){
+                List<BigDecimal> list = new ArrayList<>();
+                list.add(BigDecimal.valueOf(10));
+                list.add(BigDecimal.valueOf(20));
+                list.add(BigDecimal.valueOf(30));
+                list.add(BigDecimal.valueOf(20));
+                list.add(BigDecimal.valueOf(20));
+                list.add(BigDecimal.valueOf(20));
+                list.add(BigDecimal.valueOf(20));
+                list.add(BigDecimal.valueOf(20));
+
+                PriceRequest priceRequest = new PriceRequest();
+                priceRequest.setPriceList(list);
+
+
+                PriceResponse priceResponse = priceService.sumOfALlValues(priceRequest);
+
+                assertEquals(BigDecimal.valueOf(160).setScale(2, RoundingMode.CEILING),priceResponse.getPrice());
+            }
+
+
+            @Test
+            public void CalculateNegativePrice(){
+                List<BigDecimal> list = new ArrayList<>();
+                list.add(BigDecimal.valueOf(-10));
+                list.add(BigDecimal.valueOf(-20));
+                list.add(BigDecimal.valueOf(-30));
+                list.add(BigDecimal.valueOf(20));
+                list.add(BigDecimal.valueOf(20));
+                list.add(BigDecimal.valueOf(20));
+                list.add(BigDecimal.valueOf(20));
+                list.add(BigDecimal.valueOf(20));
+
+                PriceRequest priceRequest = new PriceRequest();
+                priceRequest.setPriceList(list);
+
+
+                PriceResponse priceResponse = priceService.sumOfALlValues(priceRequest);
+
+                assertEquals(BigDecimal.valueOf(40).setScale(2, RoundingMode.CEILING),priceResponse.getPrice());
+            }
+
 
 
 
     @Test
-    void testPriceSum() {
-        assertEquals( priceService.sumOfALlValues(priceList),105);
+    public void CalculatePriceOverflow(){
+        List<BigDecimal> list = new ArrayList<>();
+        list.add(BigDecimal.valueOf(Double.MAX_VALUE));
+        list.add(BigDecimal.valueOf(Double.MAX_VALUE));
+
+
+        PriceRequest priceRequest = new PriceRequest();
+        priceRequest.setPriceList(list);
+
+
+        PriceResponse priceResponse = priceService.sumOfALlValues(priceRequest);
+
+        assertEquals(BigDecimal.valueOf(Double.MAX_VALUE).add(BigDecimal.valueOf(Double.MAX_VALUE)).setScale(2, RoundingMode.CEILING),priceResponse.getPrice());
     }
-
-
-
 }
